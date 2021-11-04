42e02d005e10d0b12c32dbf93ff471fc46657a08
artazidele <77989249+artazidele@users.noreply.github.com>
2021-11-04 10:20:48 +0200

    upload one picture

--- app/src/main/java/com/example/thecatapiapplication/MainActivity.kt
+++ app/src/main/java/com/example/thecatapiapplication/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.thecatapiapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
@@ -24,8 +26,50 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         for (number in 1..20) {
             getMyData()
+            if (number == 1) {
+                loadMyData()
+            }
+
+
+
         }
 //        getMyData()
+    }
+    private fun loadMyData() {
+        val retrofitBuilder = Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .baseUrl(BASE_URL)
+            .build()
+            .create(ApiInterface::class.java)
+        val retrofitData = retrofitBuilder.getData()
+        retrofitData.enqueue(object : Callback<List<MyDataItem>?>{
+            override fun onResponse(
+                call: Call<List<MyDataItem>?>,
+                response: Response<List<MyDataItem>?>
+            ) {
+                val responseBody = response.body()!!
+                var urlString = ""
+                for (myData in responseBody) {
+                    urlString = myData.url
+                }
+//                findViewById<TextView>(R.id.just_text).text = myStringBuilder
+
+                val productImage: ImageView = findViewById(R.id.just_image)
+                Glide.with(this@MainActivity)
+                    .load(urlString)
+                    .into(productImage)
+//                val myStringBuilder = StringBuilder()
+//                for (myData in responseBody) {
+//                    myStringBuilder.append(myData.url)
+//                    myStringBuilder.append("\n")
+//                }
+//                findViewById<TextView>(R.id.just_text).text = myStringBuilder
+            }
+
+            override fun onFailure(call: Call<List<MyDataItem>?>, t: Throwable) {
+                Log.d("MainActivity", "onFailure: "+t.message)
+            }
+        })
     }
     private fun getMyData() {
         val retrofitBuilder = Retrofit.Builder()
@@ -52,8 +96,5 @@ class MainActivity : AppCompatActivity() {
                 Log.d("MainActivity", "onFailure: "+t.message)
             }
         })
-
-
-
     }
 }
